c87f9adb0e3042a1dd1f7331828586159c540fd5
hnlee <roar5334@gmail.com>
2017-06-03 03:30:29 +0900

    Commit

--- MyStudio/Program.cs
+++ MyStudio/Program.cs
@@ -11,6 +11,8 @@ namespace MyStudio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Check Update Contents");
         }
     }
 }
