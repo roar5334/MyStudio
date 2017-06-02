6dc0c2fbd25768bc8bb9d86a92d78eba3fe29b1a
hnlee <roar5334@gmail.com>
2017-06-03 03:35:55 +0900

    test

--- MyStudio/Program.cs
+++ MyStudio/Program.cs
@@ -11,7 +11,7 @@ namespace MyStudio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+             
             Console.WriteLine("Check Update Contents");
         }
     }
